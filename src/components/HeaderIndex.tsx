991f0215362d8f67a17f355c2fc55a7bf05c9449
Laecio Filho <141887207+LaecioFillho@users.noreply.github.com>
2025-02-25 21:52:14 -0300

    feat: edit header

--- src/components/HeaderIndex.tsx
+++ src/components/HeaderIndex.tsx
@@ -9,17 +9,17 @@ export default function headerIndex(){
             <section className="header">
                 <aside className="back-header">
                     <nav className="nav1">
-                        <a href="#iabaut">Engenheiro</a>
                         <a href="#iabaut">Sobre a Empresa</a>
+                        <a href="#iabaut">Engenheiro</a>
                         <a href="#iprojects">Projetos</a>
                         <a href="#iservices">Serviços</a>
                     </nav>
 
-                    <nav className="nav2">
+                    {/* <nav className="nav2">
                         <a href="#">Tecnologias</a>
                         <a href="#">Área do Cliente </a>
                         <a href="#icontact">Contatos</a>
-                    </nav>
+                    </nav> */}
 
                 </aside>
             </section>
